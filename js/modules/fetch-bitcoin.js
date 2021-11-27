8c2913a3465cb732da26fe0bc8e81242abce123f
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2021-11-27 13:16:51 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitCoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitCoin(url, target) {
+  fetch(url)
     .then((res) => res.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
+      const btcPreco = document.querySelector(target);
       // A quantidade dividida é a que vai ser doada
       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
     }).catch((error) => {
