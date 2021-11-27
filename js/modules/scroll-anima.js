e8646d314c4e238110e5dd5e3bc403fa4228cbfe
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2021-11-27 14:13:27 -0300

    animação ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,26 +3,43 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;// vai pegar á altura da tela é multipliacar 0.6 que é 6%
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      // getBoundingClientRect().top vai pegar á altura que o elemento esta do topo da pánigina
-      const sectionTop = section.getBoundingClientRect().top; 
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
+  // Pega a distância de cada item em relação ao topo do site 
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop; 
+      return { 
+        element: section, 
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
 
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) { // casso se eu quiser remover esta classe para ativa ela denovo
-        section.classList.remove('ativo');
+  // verifica a distância em cada objeto
+  // em relação ao scroll do site 
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) { // casso se eu quiser remover esta classe para ativa ela denovo
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
     return this;
+  }
+
+  // Remove o event de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
