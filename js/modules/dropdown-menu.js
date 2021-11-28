ca314222d64bfccdbb3a3514e66e6c33482e87d7
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2021-11-28 09:44:12 -0300

    dropedownmenu refatorado

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,21 +1,40 @@
 import outsideClick from './outsideclick.js'; 
 
 // função tousch para mobile
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+
+    this.events = ['touchstart', 'click'];
+    this.activeClass = 'active';
+    this.activeDropdownMenus = this.activeDropdownMenus.bind(this);
+  }
   
-  function handleClick(event) {
+  // Ativa o dropdownmenu e adicionar 
+  // a função que obsevar o clique fora dele
+  activeDropdownMenus(event) {
     event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove('active');
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    // o evento click demora 300ms para começar, já o touchstart é imediato
-    ['touchstart', 'click'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  // adiciona os eventos ao dropdownmenu
+  addDropdownMenuEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      // o evento click demora 300ms para começar, já o touchstart é imediato
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenus);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.addDropdownMenuEvent.length) {
+      this.addDropdownMenuEvent();
+    }
+    return this;
+  }
 }
